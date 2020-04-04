14f2d2b46b6bd2d942bbd26d87cb2bb0c8f94a66
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2020-04-04 18:32:29 +0300

    finish architecture

--- generate_embeddings.py
+++
@@ -1,8 +0,0 @@
-import fasttext
-
-# model = fasttext.train_supervised('./discord.txt', lr=0.2, epoch=18, thread=4, pretrainedVectors='./crawl-300d-2M.vec')
-# model.save_model("./retrained/latest.bin")
-
-import fasttext
-model = fasttext.train_unsupervised('./discord.txt', "cbow")
-model.save_model("./discord.bin")
